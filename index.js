2292bbbcf1c62da5bb3ccd3974877a78edbb6cdc
Stig Dreyer <stig.dreyer@busy-brain.de>
2013-08-06 16:29:25 +0100

    updated dependencies & updated code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 if (! process.env.NODE_ENV) process.env.NODE_ENV = 'production';
 
 var AuthClient = require('crp-auth-client');
-var JobClient = require('crp-job-client');
-var JobProducerClient = require('crp-job-producer-client');
+var TaskClient = require('crp-task-client');
+var TaskProducerClient = require('crp-task-producer-client');
 
 var fs = require('fs');
 
@@ -10,35 +10,35 @@ var fs = require('fs');
 AuthClient.login('email', 'password', function(err, credential) {
 	if (err) throw err;
 
-	//Options for creating job and dataunit stream
+	//Options for creating task and dataunit stream
   	var options = {
 	   bid: 1,
 	   program: fs.readFileSync('./lib/program.min.js', 'utf8'), //Reads source code for Run(data) function from file
 	   credential: credential
 	};
 
-	createJob(options);
+	createTask(options);
 });
 
-function createJob(options) {
+function createTask(options) {
 
-	var jobClient = JobClient({
+	var taskClient = TaskClient({
     	credential: options.credential
     });
 
-	//Create CrowdProcess job
-	jobClient.jobs.create({
+	//Create CrowdProcess task
+	taskClient.tasks.create({
     	bid: options.bid,
     	program: options.program
-    }, afterJobCreated);
+    }, afterTaskCreated);
 
-	function afterJobCreated(err, job) {
+	function afterTaskCreated(err, task) {
 	    if (err) throw err;
 
 	    //Create dataunit stream to send dataunits directly to CrowdProcess
-	    var stream = JobProducerClient({
+	    var stream = TaskProducerClient({
 	      credential: options.credential,
-	      jobId: job._id
+	      taskId: task._id
 	    });
 
 	    //Catch faults or errors emited by CrowdProcess
@@ -76,13 +76,13 @@ function createJob(options) {
 	    	//Did not receive all dataunits
 	    	if (sent != received) console.error('Only received %d of the %d dataunits sent.', received, sent);
 	    	
-	    	//Calculate average of all received computations
-	    	subjectDiesNotSpinning = subjectDies[0] / received;
-	    	subjectDiesSpinning = subjectDies[1] / received;
+	    	//Calculate average percentage of all received computations
+	    	subjectDiesNotSpinning = subjectDies[0] / (received * trials) * 100;
+	    	subjectDiesSpinning = subjectDies[1] / (received * trials) * 100;
 
 	    	console.log("Results for Monte Carlo Simulation with %d received computations à %d trials", received, trials);
-    		console.log("Subject died in %d % of the trials when choosing NOT to spin the chamber", subjectDiesNotSpinning);
-    		console.log("Subject died in %d % of the trials when choosing to spin the chamber", subjectDiesSpinning);
+    		console.log("Subject died in %d % of the trials when choosing NOT to spin the chamber", subjectDiesNotSpinning.toFixed(2));
+    		console.log("Subject died in %d % of the trials when choosing to spin the chamber", subjectDiesSpinning.toFixed(2));
     		console.log("Subject should %s in order to have a higher possibility to stay alive", subjectDiesNotSpinning < subjectDiesSpinning ? 'NOT spin' : 'spin');
 
 	    });
